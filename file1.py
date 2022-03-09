2e6750d23981b817c6df52d5792ddaec249fb7ba
Sebastian Bischoff <sebastian@salzreute.de>
2022-03-09 12:23:22 +0100

    Use cubic

--- file1.py
+++ file1.py
@@ -1,10 +1,11 @@
 from time import time
+import numpy as np
 
 i_squared = []
 
 tic = time.time()
 for i in range(10):
-    i_squared.append(i**2)
+    i_squared.append(i**3)
 toc = time.time()
 
 print(f"time elapsed: {toc - tic}")
